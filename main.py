f002e8acf24075941cf73f5abb1ee9eb749dd84c
AM <mffam777@gmail.com>
2018-12-06 13:54:21 -0500

    spaces correstinon

--- main.py
+++ main.py
@@ -6,8 +6,17 @@
 # Assignment: user_signedup
 # Due date: December 17, 2018 @ 21:00
 
-
+# import modules for ther project
 from flask import Flask
+import os
+import jinja2
+
+# add absolute path in the filesystem on which the program is running.
+template_dir = os.path.join(os.path.dirname(__file__),
+                            'templates')
+# initialize jinja templating engine
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
