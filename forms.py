7689f729567f29dfa9d2328e0d7fcc34923cda06
AM <mffam777@gmail.com>
2018-12-15 02:28:19 -0500

    Created forms.py

---
+++ forms.py
@@ -0,0 +1,16 @@
+# Import Flask-WTF functions
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+# Pending Delete
+from wtforms import TextField
+from wtforms import Form, BooleanField, TextField, validators
+
+class SignupForm(Form):
+
+    first_name = StringField('First Name', validators=[DataRequired("Please enter your First Name.")])
+    last_name = StringField('Last Name', validators=[DataRequired("Please enter your Last Name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
+    submit = SubmitField('Sign up')
+
