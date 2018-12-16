e2e7e7090d17670384508fb4f200fc2b15a75649
AM <mffam777@gmail.com>
2018-12-16 15:05:16 -0500

    corrected class SignupForm

--- forms.py
+++ forms.py
@@ -2,15 +2,18 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-# Pending Delete
-from wtforms import TextField
-from wtforms import Form, BooleanField, TextField, validators
+
 
 class SignupForm(Form):
+    #pending check
+    user_name = StringField('User Name', validators=[DataRequired("Please enter your User Name.")])
+    user_password = PasswordField('Password', validators=[DataRequired(
+        "Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])   
 
-    first_name = StringField('First Name', validators=[DataRequired("Please enter your First Name.")])
-    last_name = StringField('Last Name', validators=[DataRequired("Please enter your Last Name.")])
-    email = StringField('E-mail', validators=[DataRequired("Please enter your E-mail address."), Email("Please enter your email address.")])
-    password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
+    user_vpwd = PasswordField('Password', validators=[DataRequired(
+        "Password verification doesn't match.")])
+
+    user_email = StringField('E-mail', validators=[DataRequired("Please enter your E-mail address."), Email("Please enter your email address.")])
+    
     submit = SubmitField('Sign up')
 
