a2c94e8ce8055d4a2216e19694a8968b10895140
AM <mffam777@gmail.com>
2018-12-17 01:37:03 -0500

    added validators

--- main.py
+++ main.py
@@ -9,17 +9,17 @@
 # Due date: December 17, 2018 @ 21:00
 
 # import modules need it for the project
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
-from wtforms.validators import InputRequired, Email, Length
-from wtforms import StringField, PasswordField, SubmitField
-import cgi
-import os
+from wtforms.validators import InputRequired, Email, Length, AnyOf
+from wtforms import StringField, PasswordField, SubmitField, Form, validators
+from wtforms.fields.html5 import EmailField
+#import cgi
+#import os
 import jinja2
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email, Length, DataRequired
+
 
 
 app = Flask(__name__)
@@ -28,19 +28,24 @@ app.config['SECRET_KEY'] = 'development-key'
 
 class SignupForm(FlaskForm):
     #pending check
-    username = StringField('username', validators=[
-                       InputRequired("A username is require!")])
+    username = StringField('username', validators=[InputRequired
+    (message='A username is required!'), 
+    Length(min=3, max=20, message='Username must be between 3 to 20 characters long!')])
 
-    password = PasswordField('password', validators=[InputRequired(
-        "Password is require!"), validators.EqualTo('confirm', message='Passwords must match')])
+    password = PasswordField('password', validators=[InputRequired
+    (message="Password is require!"), Length(
+        min=3, max=20, message=
+        'Password must be between 3 to 20 characters long, do not use space!'), 
+        validators.EqualTo(
+        'confirm', message='Passwords must match')])
 
     confirm = PasswordField('Repeat Password')
 
-    email = StringField('E-mail', validators=[InputRequired(
-        "Please enter your E-mail address."), Email("Please enter your email address.")])
+    #email = StringField('E-mail')
+    email = EmailField('Email address', [
+                       validators.DataRequired(), validators.Email()])
 
     submit = SubmitField('Sign up')
-
 
 
 @app.route("/index", methods=['GET','POST'])
@@ -49,16 +54,14 @@ def signup():
     form = SignupForm()
 
     if form.validate_on_submit():
-        return 'Sumitted!'
+        return ' <h1> Welcome {}! ' . format(form.username.data, form.password.data)
 
-    return render_template('signup.html', form=form)
-
-
+    return render_template('index.html', form=form)
 
 
 
-###
-   
+
+
 
 
 
