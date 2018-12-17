83310fcbf4dba52cb76bb4073509234c9d486b0a
AM <mffam777@gmail.com>
2018-12-17 15:15:37 -0500

    edited coding to email validation

--- main.py
+++ main.py
@@ -11,14 +11,16 @@
 # import modules need it for the project
 from flask import Flask, render_template
 
-from wtforms.validators import InputRequired, Email, Length, AnyOf
+from wtforms.validators import InputRequired, DataRequiremed, Length,  Email, EqualTo
 from wtforms import StringField, PasswordField, SubmitField, Form, validators
 from wtforms.fields.html5 import EmailField
+from email_validator import validate_email, EmailNotValidError
+from flask_wtf import FlaskForm
 #import cgi
 #import os
+import sys
 import jinja2
 
-from flask_wtf import FlaskForm
 
 
 
@@ -27,7 +29,7 @@ app.config['SECRET_KEY'] = 'development-key'
 
 
 class SignupForm(FlaskForm):
-    #pending check
+    
     username = StringField('username', validators=[InputRequired
     (message='A username is required!'), 
     Length(min=3, max=20, message='Username must be between 3 to 20 characters long!')])
@@ -41,11 +43,12 @@ class SignupForm(FlaskForm):
 
     confirm = PasswordField('Repeat Password')
 
-    #email = StringField('E-mail')
-    email = EmailField('Email address', [
-                       validators.DataRequired(), validators.Email('Please enter a valid E-Mail')])
+    email = StringField('Email', [validators.Optional(), validators.input_required('@')])
 
     submit = SubmitField('Sign up')
+
+
+
 
 
 @app.route("/index", methods=['GET','POST'])
@@ -57,16 +60,6 @@ def signup():
         return ' <h1> Welcome {}! ' . format(form.username.data, form.password.data)
 
     return render_template('index.html', form=form)
-
-
-
-
-
-
-
-
-
-
 
 
 # statement app.run runs the app on local server
